5586fbf47bdd1a02cd8cf9ef1c3fd7bbde90dee1
Felipe Skinner <felipeskinner@gmail.com>
2015-04-30 00:40:11 -0300

    Removing unnecessary tags/comments and fixing html path on Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -18,10 +18,6 @@ function reportChange(event){
   console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 }
 
-// this task wil watch for changes
-// to js, html, and css files and call the
-// reportChange method. Also, by depending on the
-// serve task, it will instantiate a browserSync session
 gulp.task('watch', ['serve'], function() {
   gulp.watch(paths.source, ['lint', browserSync.reload]).on('change', reportChange);
   gulp.watch(paths.html, browserSync.reload).on('change', reportChange);
